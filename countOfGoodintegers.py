c72812d3d51a6a6f0eb82eb715e437c42b865fc7
Ashwani <ashwani90125@gmail.com>
2025-05-25 11:38:30 +0530

    Count good integers

---
+++ countOfGoodintegers.py
@@ -0,0 +1,33 @@
+'''
+You are given two positive integers n and k.
+
+An integer x is called k-palindromic if:
+
+x is a palindrome.
+x is divisible by k.
+An integer is called good if its digits can be rearranged to form a k-palindromic integer. For example, for k = 2, 2020 can be rearranged to form the k-palindromic integer 2002, whereas 1010 cannot be rearranged to form a k-palindromic integer.
+
+Return the count of good integers containing n digits.
+
+Note that any integer must not have leading zeros, neither before nor after rearrangement. For example, 1010 cannot be rearranged to form 101.
+
+ 
+
+Example 1:
+
+Input: n = 3, k = 5
+
+Output: 27
+
+Explanation:
+
+Some of the good integers are:
+
+551 because it can be rearranged to form 515.
+525 because it is already k-palindromic.
+
+'''
+
+def countGoodIntegers(n, k):
+    # very difficult 
+    # really dont know what leetcode wants from this
