8767e007d745e0acfa78965a3948b55690baddc2
Ashwani <ashwani90125@gmail.com>
2025-06-04 15:18:09 +0530

    Montonoic stack problem

---
+++ monotonic/stack/monotonicStack.py
@@ -0,0 +1,15 @@
+"""
+
+monotonic stack maintains items in sorted order
+like in increasing and decreasing order
+
+It will involve problems like
+Next greater element
+Previous smaller element
+Stock spans
+Histogram Area
+temerature Comparisons
+
+"""
+
+
