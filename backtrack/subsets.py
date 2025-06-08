7bc80f3dc1c9f435e1a4245b724283c782f5c610
Ashwani <ashwani90125@gmail.com>
2025-06-08 12:10:00 +0530

    Backtracking problem

---
+++ backtrack/subsets.py
@@ -0,0 +1,18 @@
+"""
+
+Get subsets of all the problems
+
+"""
+
+def subSets(nums):
+    result = []
+
+    def backtrack(start, path):
+        result.append(path[:])
+        for i in range(start, len(nums)):
+            path.append(nums[i])
+            backtrack(i+1, path)
+            path.pop()
+
+    backtrack(0, [])
+    return result
