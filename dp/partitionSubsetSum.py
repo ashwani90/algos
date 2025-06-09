bcc21365a48e6e14d82c1ff3c754da97229412a4
Ashwani <ashwani90125@gmail.com>
2025-06-09 13:35:48 +0530

    DP algo

---
+++ dp/partitionSubsetSum.py
@@ -0,0 +1,23 @@
+"""
+
+Partition equal subset sum
+
+"""
+
+def can_partition(nums):
+    total = sum(nums)
+    if total%2 != 0:
+        return False
+
+    target = total//2
+    dp = set([0])
+    for num in nums:
+        next_dp = dp.copy()
+        for t in dp:
+            if t+num == target:
+                return True
+
+            next_dp.add(t+num)
+        dp = next_dp
+    
+    return target in dp
