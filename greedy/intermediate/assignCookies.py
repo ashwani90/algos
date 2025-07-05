2e70546c18befcdbc80fa64fe7305c473b9fb1d5
Ashwani <ashwani90125@gmail.com>
2025-07-05 15:56:58 +0530

    Cookie question

---
+++ greedy/intermediate/assignCookies.py
@@ -0,0 +1,15 @@
+"""
+Assign cookies
+
+"""
+
+def cookies(g, s):
+    g.sort()
+    s.sort()
+    i = j = 0
+    while i < len(g) and j < len(s):
+        if s[j] >= g[i]:
+            i += 1
+        
+        j += 1
+    return i
