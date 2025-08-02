4f120917ea94ac2cc4e7b6c43f65fe93246bee11
Ashwani <ashwani90125@gmail.com>
2025-08-03 00:42:08 +0530

    Add sliding window problem

---
+++ slidingWindow/beginner/findAllAnagrams.py
@@ -0,0 +1,19 @@
+"""
+Problem: Find all start indices of p's anagrams in s.
+
+"""
+
+def find_anagrams(s, p):
+    res = []
+    p_count = Counter(p)
+    s_count = Counter()
+
+    for i in range(len(s)):
+        s_count[s[i]] += 1
+        if i >= len(p):
+            s_count[s[i - len(p)]] -= 1
+            if s_count[s[i - len(p)]] == 0:
+                del s_count[s[i - len(p)]]
+        if s_count == p_count:
+            res.append(i - len(p) + 1)
+    return res
