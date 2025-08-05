f23150c55ba3b446d5f80d0d128e39438910c239
Ashwani <ashwani90125@gmail.com>
2025-08-06 00:13:28 +0530

    DS problem

---
+++ slidingWindow/intermediate/maxConsecutiveOnes3.py
@@ -0,0 +1,19 @@
+"""
+
+Problem: Variation where you flip k 0s to 1s to get the longest 1s subarray.
+"""
+
+def longest_ones(nums, k):
+    left = 0
+    max_len = 0
+    zero_count = 0
+
+    for right in range(len(nums)):
+        if nums[right] == 0:
+            zero_count += 1
+        while zero_count > k:
+            if nums[left] == 0:
+                zero_count -= 1
+            left += 1
+        max_len = max(max_len, right - left + 1)
+    return max_len
