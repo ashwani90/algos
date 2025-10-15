6a7dc4cca1b191b256ee8588d97c7a7b005d85a4
Ashwani <ashwani90125@gmail.com>
2025-10-16 00:20:21 +0530

    Flood fill bfs solution

---
+++ bfs/floodFill.py
@@ -0,0 +1,33 @@
+"""
+You are given an image represented by an m x n grid of integers image, where image[i][j] represents the pixel value of the image. You are also given three integers sr, sc, and color. Your task is to perform a flood fill on the image starting from the pixel image[sr][sc].
+
+To perform a flood fill:
+
+Begin with the starting pixel and change its color to color.
+Perform the same process for each pixel that is directly adjacent (pixels that share a side with the original pixel, either horizontally or vertically) and shares the same color as the starting pixel.
+Keep repeating this process by checking neighboring pixels of the updated pixels and modifying their color if it matches the original color of the starting pixel.
+The process stops when there are no more adjacent pixels of the original color to update.
+Return the modified image after performing the flood fill.
+
+"""
+
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        # Habit of using grid in these problems
+        grid = image
+        m, n = len(grid), len(grid[0])
+        color_to_check = grid[sr][sc]
+        if color_to_check == color:
+            return grid
+
+        def dfs(i,j):
+            if i < 0 or j < 0 or i >= m or j >= n or grid[i][j] != color_to_check:
+                return
+            grid[i][j] = color
+            dfs(i+1, j)
+            dfs(i-1, j)
+            dfs(i, j+1)
+            dfs(i, j-1)
+
+        dfs(sr, sc)
+        return grid
