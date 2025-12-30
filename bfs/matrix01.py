9d074d7ffef68a2a085ff97acf0c6bb056de6801
Ashwani <ashwani90125@gmail.com>
2025-12-30 17:52:21 +0530

    BFS problem practice

---
+++ bfs/matrix01.py
@@ -0,0 +1,30 @@
+"""
+Given an m x n binary matrix mat, return the distance of the nearest 0 for each cell.
+
+The distance between two cells sharing a common edge is 1.
+
+"""
+
+class Solution:
+    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        grid = mat
+        rows, cols = len(grid), len(grid[0])
+        dist = [[-1]*cols for _ in range(rows)]
+        queue = deque()
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 0:
+                    dist[r][c] = 0
+                    queue.append((r,c))
+        
+        directions = [(1,0), (0,1), (0, -1), (-1, 0)]
+        while queue:
+            r, c = queue.popleft()
+            for dr, dc in directions:
+                nr, nc = r+dr, c+dc
+                if 0 <= nr < rows and 0 <= nc < cols and dist[nr][nc] == -1:
+                    dist[nr][nc] = dist[r][c] + 1
+                    queue.append((nr, nc))
+        
+        return dist
