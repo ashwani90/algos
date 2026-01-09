d5d8d341e84472d021d84d15199e6721d9aef05b
Ashwani <ashwani90125@gmail.com>
2026-01-10 00:04:07 +0530

    Combinatorics problem

---
+++ combinatorics/subsetsOfSet.py
@@ -0,0 +1,9 @@
+"""
+
+Find the total number of subsets of a set with n elements.
+"""
+
+# same solution as the item could or could not be part of the subset
+# so two choices here as well - 2 ** n
+
+return 2**n
